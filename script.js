65f654a8c9e540fc1d08a7d08e46060875f01fc6
Satyam Chaudhary <chaudharysatyam313@gmail.com>
2023-08-09 19:57:44 +0530

    added basic svg backgroung blob not added yet

--- script.js
+++ script.js
@@ -19,9 +19,10 @@
              }
         }     
     }
-
+   
     window.addEventListener("load", callbackFunc);
     window.addEventListener("resize", callbackFunc);
     window.addEventListener("scroll", callbackFunc);
+    
 
 })()
